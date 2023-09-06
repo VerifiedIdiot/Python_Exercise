990b141921884f06b032d77c0c558bf818ed7b8c
lastofyears21 <lastofyears21@gmail.com>
2023-09-06 11:05:25 +0900

    파이선 최초 깃 푸시

---
+++ 기본.py
@@ -0,0 +1,25 @@
+# "주소 : "서울시 강남구 역삼동 KH정보교육원"
+# "이름 : "곰돌이사육사"
+# "이메일 : jks2024@megastudy.com"
+
+name = "곰돌이사육사"
+email = "jks2024"
+age = 25
+addr = "서울시 강남구 역삼동 KH정보교육원"
+
+print(f"""
+주소 : {addr}
+이름 : {name}
+나이 : {age}
+이메일 : {email}@gmail.com
+""")
+
+
+# 파이썬은 불리언 값이 대문자로 시작
+
+isAdult = True
+
+if isAdult :
+    print("나는 성인 입니다.")
+else :
+    print("나는 성인이 아닙니다.")
